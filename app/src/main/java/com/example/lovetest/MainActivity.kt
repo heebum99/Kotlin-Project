7152dfb46cc4578b98b76768309ebf1ec2195fda
heebum99 <heebum9955@naver.com>
2023-01-13 14:25:16 +0900

    Main_Fragment

--- app/src/main/java/com/example/lovetest/MainActivity.kt
+++ app/src/main/java/com/example/lovetest/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.lovetest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//activity는 fragment를 담아주는 틀 역할
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
