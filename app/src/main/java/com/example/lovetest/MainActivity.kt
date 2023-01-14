d7087f4bea7e3cc934d865bd84f209d550ce7794
heebum99 <heebum9955@naver.com>
2023-01-14 19:55:29 +0900

    Project_Complete

--- app/src/main/java/com/example/lovetest/MainActivity.kt
+++ app/src/main/java/com/example/lovetest/MainActivity.kt
@@ -2,12 +2,18 @@ package com.example.lovetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 
 //activity는 fragment를 담아주는 틀 역할
-
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = findNavController(R.id.nav_host_fragment)
     }
 }
--- app/src/main/java/com/example/lovetest/fragment/SelectionFragment.kt
+++ app/src/main/java/com/example/lovetest/fragment/SelectionFragment.kt
@@ -5,30 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.lovetest.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import kotlinx.android.synthetic.main.fragment_selection.*
 
 /**
  * A simple [Fragment] subclass.
  * Use the [SelectionFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SelectionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class SelectionFragment : Fragment() , View.OnClickListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +29,39 @@ class SelectionFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_selection, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SelectionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SelectionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        btn_back.setOnClickListener(this)
+        option_1.setOnClickListener(this)
+        option_2.setOnClickListener(this)
+        option_3.setOnClickListener(this)
+        option_4.setOnClickListener(this)
+
     }
+
+
+    override fun onClick(v: View?) {
+
+        when(v?.id){
+            R.id.option_1 -> {navigateWithIndex(1)}
+            R.id.option_2 -> {navigateWithIndex(2)}
+            R.id.option_3 -> {navigateWithIndex(3)}
+            R.id.option_4 -> {navigateWithIndex(4)}
+            R.id.btn_back -> {
+                navController.popBackStack()
+            }
+
+        }
+    }
+
+    fun navigateWithIndex(index : Int){
+
+        val bundle = bundleOf("index" to index)
+        navController.navigate(R.id.action_selectionFragment_to_resultFragment,bundle)
+    }
+
+
 }
